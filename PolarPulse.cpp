27d26a12df630e954bc0a1401d8066d9827aad7d
Dieter Niklaus <dieter.niklaus@contractors.roche.com>
2016-05-15 13:25:29 +0200

    pulse sensor impl.

--- PolarPulse.cpp
+++ PolarPulse.cpp
@@ -5,16 +5,110 @@
  *      Author: niklausd
  */
 
+#include <Timer.h>
+#include <Arduino.h>
+
 #include "PolarPulse.h"
 
-PolarPulse::PolarPulse()
-{
-  // TODO Auto-generated constructor stub
+const bool PolarPulse::IS_POS_LOGIC = false;
+const bool PolarPulse::IS_NEG_LOGIC = true;
+const int  PolarPulse::PLS_NC       = -1;
+const int  PolarPulse::IND_NC       = -1;
 
+const int  PolarPulse::s_defaultPulsePollTimeMillis = 5;
+const int  PolarPulse::s_defaultReportIntervalMillis = 15000;
+
+//-----------------------------------------------------------------------------
+
+class PollingTimerAdapter : public TimerAdapter
+{
+private:
+  PolarPulse* m_pulseSesor;
+  bool m_lastWasPulseHigh;
+public:
+  PollingTimerAdapter(PolarPulse* pulseSesor)
+  : m_pulseSesor(pulseSesor)
+  , m_lastWasPulseHigh(false)
+  { }
+
+  void timeExpired()
+  {
+
+  }
+};
+
+//-----------------------------------------------------------------------------
+
+class ReportTimerAdapter : public TimerAdapter
+{
+private:
+  PolarPulse* m_pulseSesor;
+public:
+  ReportTimerAdapter(PolarPulse* pulseSesor)
+  : m_pulseSesor(pulseSesor)
+  { }
+
+  void timeExpired()
+  {
+
+  }
+};
+
+//-----------------------------------------------------------------------------
+
+PolarPulse::PolarPulse(int pulsePin, int indicatorPin, bool isPulsePinNegativeLogic, PolarPulseAdapter* adapter)
+: m_pollingTimer(new Timer(new PollingTimerAdapter(this), Timer::IS_RECURRING, s_defaultPulsePollTimeMillis))
+, m_reportTimer(new Timer(new ReportTimerAdapter(this), Timer::IS_RECURRING, s_defaultReportIntervalMillis))
+, m_adapter(adapter)
+, m_isPulsePinNegativeLogic(isPulsePinNegativeLogic)
+, m_isActive(false)
+, m_pulsePin(pulsePin)
+, m_indicatorPin(indicatorPin)
+{
+  if (0 <= m_pulsePin)
+  {
+    pinMode(m_pulsePin, INPUT);
+    digitalWrite(m_pulsePin, m_isPulsePinNegativeLogic ? HIGH : LOW); // pull
+  }
+  if (0 <= m_indicatorPin)
+  {
+    pinMode(m_indicatorPin, OUTPUT);
+    digitalWrite(m_indicatorPin, m_isActive);
+  }
 }
 
 PolarPulse::~PolarPulse()
 {
-  // TODO Auto-generated destructor stub
+  delete m_pollingTimer->adapter();
+  m_pollingTimer->attachAdapter(0);
+
+  delete m_pollingTimer;
+  m_pollingTimer = 0;
+
+  delete m_reportTimer->adapter();
+  m_reportTimer->attachAdapter(0);
+
+  delete m_reportTimer;
+  m_reportTimer = 0;
 }
 
+PolarPulseAdapter* PolarPulse::adapter()
+{
+  return m_adapter;
+}
+
+void PolarPulse::attachAdapter(PolarPulseAdapter* adapter)
+{
+  m_adapter = adapter;
+}
+
+bool PolarPulse::isPulseActive()
+{
+  bool active = false;
+  if (0 <= m_pulsePin)
+  {
+    active = digitalRead(m_pulsePin);
+    active = (m_isPulsePinNegativeLogic ? !active : active);
+  }
+  return active;
+}
--- PolarPulse.h
+++ PolarPulse.h
@@ -8,11 +8,96 @@
 #ifndef COMP_POLARPULSE_POLARPULSE_H_
 #define COMP_POLARPULSE_POLARPULSE_H_
 
+class Timer;
+
+//-----------------------------------------------------------------------------
+
+class PolarPulseAdapter
+{
+protected:
+  PolarPulseAdapter() { }
+  virtual ~PolarPulseAdapter() { }
+
+private:  // forbidden functions
+  PolarPulseAdapter(const PolarPulseAdapter& src);              // copy constructor
+  PolarPulseAdapter& operator = (const PolarPulseAdapter& src); // assignment operator
+};
+
+//-----------------------------------------------------------------------------
+
+/**
+ * Polar Heart Rate sensor model.
+ * The Polar Heart Rate sensor (https://www.adafruit.com/product/1077) events are recognized.
+ * The events are counted, the number of events are reported any 15 seconds, extrapolated the rate per minute.
+ * The puls input pin can be configured, so also it's logic (rising or falling edge).
+ * A pulse indicator output can be defined.
+ */
 class PolarPulse
 {
 public:
-  PolarPulse();
+  /**
+   * Constructor.
+   * @param pulsePin                Arduino Pin where the pulse sensor is connected to; default: PLS_NC (not connected)
+   * @param indicatorPin            Arduino Pin where the (LED) status indication is connected to; default: IND_NC (not connected)
+   * @param isPulsePinNegativeLogic Consider active pulse starting with a High to LOW edge (true) or LOW to HIGH edge (false);
+   *                                default: false (pulse sensor object triggering on the rising egde)
+   * @param adapter                 Inject pointer to PolarPulseAdapter object; NULL pointer: no status change notification will be sent out.
+   */
+  PolarPulse(int pulsePin = PLS_NC, int indicatorPin = IND_NC, bool isPulsePinNegativeLogic = false, PolarPulseAdapter* adapter = 0);
   virtual ~PolarPulse();
+
+  /**
+   * Retrieve pointer to currently injected adapter object.
+   * @return Pointer to currently injected PolarPulseAdapter object; NULL pointer if none is injected.
+   */
+  PolarPulseAdapter* adapter();
+
+  /**
+   * Attach adapter object.
+   * @param adapter Pointer to specific PolarPulseAdapter object; NULL pointer: no status change notification will be sent out.
+   */
+  void attachAdapter(PolarPulseAdapter* adapter);
+
+  /**
+   * Retrieve current pulse status.
+   * @return Pulse status: active (true) or inactive (false).
+   */
+  bool isPulseActive();
+
+  /**
+   * Constant for isButtonNegativeLogic parameter of the constructor (@see PolarPulse()), to create a pulse sensor object triggering on the rising egde.
+   */
+  static const bool IS_POS_LOGIC;
+
+  /**
+   * Constant for isButtonNegativeLogic parameter of the constructor (@see PolarPulse()), to create a pulse sensor object triggering on the falling egde.
+   */
+  static const bool IS_NEG_LOGIC;
+
+  /**
+   * Constant for constructor's pulsePin parameter (@see PolarPulse()), to create a pulse sensor object w/o any pulse sensor connected to a pin, i.e. useful for tests.
+   */
+  static const int PLS_NC;
+
+  /**
+   * Constant for constructor's indicatorPin parameter (@see PolarPulse()), to create a pulse sensor object w/o any indicator output connected to a pin.
+   */
+  static const int IND_NC;
+
+private:
+  Timer* m_pollingTimer;
+  Timer* m_reportTimer;
+  PolarPulseAdapter* m_adapter;
+  bool m_isPulsePinNegativeLogic;
+  bool m_isActive;
+  int m_pulsePin;
+  int m_indicatorPin;
+  static const int s_defaultPulsePollTimeMillis;
+  static const int s_defaultReportIntervalMillis;
+
+private:  // forbidden functions
+  PolarPulse(const PolarPulse& src);              // copy constructor
+  PolarPulse& operator = (const PolarPulse& src); // assignment operator
 };
 
 #endif /* COMP_POLARPULSE_POLARPULSE_H_ */
