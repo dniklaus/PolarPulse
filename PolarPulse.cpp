0ed726b3487bc632ee5d7dac042a8e7ea0f53dba
Dieter Niklaus <dieter.niklaus@contractors.roche.com>
2016-05-15 12:34:02 +0200

    initial commit with empty implementation

---
+++ PolarPulse.cpp
@@ -0,0 +1,20 @@
+/*
+ * PolarPulse.cpp
+ *
+ *  Created on: 15.05.2016
+ *      Author: niklausd
+ */
+
+#include "PolarPulse.h"
+
+PolarPulse::PolarPulse()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+PolarPulse::~PolarPulse()
+{
+  // TODO Auto-generated destructor stub
+}
+
---
+++ PolarPulse.h
@@ -0,0 +1,18 @@
+/*
+ * PolarPulse.h
+ *
+ *  Created on: 15.05.2016
+ *      Author: niklausd
+ */
+
+#ifndef COMP_POLARPULSE_POLARPULSE_H_
+#define COMP_POLARPULSE_POLARPULSE_H_
+
+class PolarPulse
+{
+public:
+  PolarPulse();
+  virtual ~PolarPulse();
+};
+
+#endif /* COMP_POLARPULSE_POLARPULSE_H_ */
